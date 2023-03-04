8876a8c812cab6890e9459912cb91215f6f69890
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-03-05 01:35:25 +0530

    chore: created project route

--- iot-dashboard/src/App.jsx
+++ iot-dashboard/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/HomePage";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/project/:projectId" element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
   );
