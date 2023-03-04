76590f71293e8bfef3dcd377d53b8f277a199218
monish karunakaran <ckmonish2000@gmail.com>
2023-03-04 15:57:04 +0530

    chore: initialized database utils

--- iot-platform-core/database/database.go
+++ iot-platform-core/database/database.go
@@ -1,1 +1,22 @@
 package databases
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (DB *gorm.DB)
+
+func Connect(){
+	DB, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+  if err != nil {
+    panic("failed to connect database")
+  }
+}
+
+
+func Migrate(){
+	DB.AutoMigrate()
+}
+
+
