be2abdeaf3c059c5fc3887fd2a96c4368ddb3187
monish karunakaran <ckmonish2000@gmail.com>
2023-03-04 17:39:33 +0530

    chore: initialized generic route

---
+++ iot-platform-core/controllers/genericRoute.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func HandleCreate(c gin.Context) {
+
+}
+
+func HandleFecthAll(c gin.Context) {
+
+}
+
+func HandleFecthById(c gin.Context) {
+
+}
+
+func HandleDeleteById(c gin.Context) {
+
+}
+
+func HandleUpdateById(c gin.Context) {
+
+}
