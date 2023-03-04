6b55c0231002fbafbbb027f2021776982a050e40
Skadarken <harishb159@gmail.com>
2023-03-05 00:01:55 +0530

    chore: Created function to validate jwt token in utils

--- iot-platform-core/utils/utils.go
+++ iot-platform-core/utils/utils.go
@@ -1,11 +1,14 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 func GenerateToken(user_id uint) (string, error) {
@@ -25,4 +28,30 @@ func GenerateToken(user_id uint) (string, error) {
 
 	return token.SignedString([]byte(os.Getenv("API_SECRET")))
 
+}
+
+func TokenValid(c *gin.Context) error {
+	tokenString := ExtractToken(c)
+	_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ExtractToken(c *gin.Context) string {
+	token := c.Query("token")
+	if token != "" {
+		return token
+	}
+	bearerToken := c.Request.Header.Get("Authorization")
+	if len(strings.Split(bearerToken, " ")) == 2 {
+		return strings.Split(bearerToken, " ")[1]
+	}
+	return ""
 }
