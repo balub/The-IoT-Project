800331594c88b41b301a1ae7466945a1c760fc78
Skadarken <harishb159@gmail.com>
2023-03-05 00:02:42 +0530

    chore: Created function to extract user_ID from jwt token

--- iot-platform-core/utils/utils.go
+++ iot-platform-core/utils/utils.go
@@ -55,3 +55,26 @@ func ExtractToken(c *gin.Context) string {
 	}
 	return ""
 }
+
+func ExtractTokenID(c *gin.Context) (uint, error) {
+
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+	return 0, nil
+}
