89871f24a4136e4c2ee9a994d6dcb2d7702f25be
monish karunakaran <ckmonish2000@gmail.com>
2023-03-04 17:10:17 +0530

    chore: completed basic routes structure setup

---
+++ iot-platform-core/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/balub/The-IoT-Project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRouter(r *gin.Engine){
+	publicRoute := r.Group("/auth")
+	protectedRoute := r.Group("/protected")
+
+	publicRoute.GET("/",controllers.HandleAuth)
+	protectedRoute.GET("/",controllers.HandleRegistration)
+
+	
+}
