53a47cd78ccedc0767299db36404335540c9f10c
Skadarken <harishb159@gmail.com>
2023-03-05 03:34:33 +0530

    Added CORS Allow Origin to routes

--- iot-platform-core/routes/routes.go
+++ iot-platform-core/routes/routes.go
@@ -11,6 +11,28 @@ func SetUpRouter(r *gin.Engine) {
 	publicRoute := r.Group("/auth")
 	protectedRoute := r.Group("/protected")
 
+	publicRoute.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	protectedRoute.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
 	protectedRoute.Use(middlewares.JwtAuthMiddleware())
 
 	publicRoute.POST("/register", controllers.HandleRegistration)
