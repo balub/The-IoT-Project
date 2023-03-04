059373fe116de729069fed01444d5d28e863e108
Jeevan Aaron <jeevan.aaron.joseph@gmail.com>
2023-03-05 01:29:33 +0530

    chore: created Sign up route

--- iot-dashboard/src/App.jsx
+++ iot-dashboard/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import SigninPage from "./pages/SigninPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   );
